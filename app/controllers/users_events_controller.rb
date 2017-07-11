129128eb5dfdf9fc95e32e6eeb407a90b3b51999
Aerika Jasso <aerikajasso@gmail.com>
2017-07-11 16:39:28 -0700

    removed unecessary views

--- app/controllers/users_events_controller.rb
+++ app/controllers/users_events_controller.rb
@@ -1,17 +1,15 @@
 class UsersEventsController < ApplicationController
+  def create
+  @user_event = UsersEvent.create(event_id: params[:event_id], user: current_user )
+    if @user_event.save 
+        redirect_to events_path, flash: {success: "You have joined the event"}
+    else
+        redirect_to events_path, flash: {danger: @user_event.errors.full_messages }
+    end
+  end
 
-	def create
-		
-		@user_event = UsersEvent.create(event_id: params[:event_id], user: current_user )
-			if @user_event.save 
-				redirect_to events_path, flash: {success: "You have joined the event"}
-			else
-				redirect_to events_path, flash: {danger: @user_event.errors.full_messages }
-			end
-	end
-
-	def destroy
-		UsersEvent.find(params[:id]).destroy
-		redirect_to events_path, flash: { warning: "You have left the event" }
-	end
+  def destroy
+    UsersEvent.find(params[:id]).destroy
+    redirect_to events_path, flash: { warning: "You have left the event" }
+  end
 end
